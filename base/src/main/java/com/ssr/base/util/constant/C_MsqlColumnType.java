679d13f63200d31d32bf8399aca079608e07103c
li372647444 <372647444@qq.com>
2017-03-15 22:11:27 +0800

    1

---
+++ base/src/main/java/com/ssr/base/util/constant/C_MsqlColumnType.java
@@ -0,0 +1,13 @@
+package com.ssr.base.util.constant;
+
+/**
+ * Created by Administrator on 2017-02-09.
+ */
+public class C_MsqlColumnType {
+
+    public static final String INTEGER ="integer" ;
+    public static final String VARCHAR ="varchar" ;
+    public static final String DATETIME ="datetime" ;
+    public static final String DOUBLE ="double" ;
+
+}
