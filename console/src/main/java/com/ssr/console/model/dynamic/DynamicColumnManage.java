8786036f491e1e8a90330770a66c3c4e096e493e
li372647444 <372647444@qq.com>
2017-03-19 20:47:03 +0800

    动态表与列表  增加字段

--- console/src/main/java/com/ssr/console/model/dynamic/DynamicColumnManage.java
+++ console/src/main/java/com/ssr/console/model/dynamic/DynamicColumnManage.java
@@ -1,12 +1,7 @@
 package com.ssr.console.model.dynamic;
 
 import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Table(name = "dynamic_column_manage")
 public class DynamicColumnManage {
@@ -15,16 +10,27 @@ public class DynamicColumnManage {
     private Integer id;
 
     /**
+     * 所属动态表Id
+     */
+    @Column(name = "table_id")
+    private Integer tableId;
+
+    /**
      * 列名
      */
     @Column(name = "column_name")
     private String columnName;
 
     /**
-     * 创建时间
+     * Mysql 数据库 列  类型
      */
-    @Column(name = "create_time")
-    private Date createTime;
+    @Column(name = "type_for_mysql")
+    private String typeForMysql;
+
+    /**
+     * 长度
+     */
+    private Integer length;
 
     /**
      * 小数点
@@ -33,10 +39,15 @@ public class DynamicColumnManage {
     private Integer decimalPoint;
 
     /**
-     * 是否自动递增
+     * 枚举值
      */
-    @Column(name = "is_autoincrement")
-    private Boolean isAutoincrement;
+    @Column(name = "enum_value")
+    private String enumValue;
+
+    /**
+     * 是否为空
+     */
+    private Boolean nullable;
 
     /**
      * 是否为主键
@@ -45,14 +56,10 @@ public class DynamicColumnManage {
     private Boolean isPrimaryKey;
 
     /**
-     * 长度
+     * 是否自动递增
      */
-    private Integer length;
-
-    /**
-     * 是否为空
-     */
-    private Boolean nullable;
+    @Column(name = "is_autoincrement")
+    private Boolean isAutoincrement;
 
     /**
      * 备注
@@ -60,16 +67,28 @@ public class DynamicColumnManage {
     private String remark;
 
     /**
-     * 所属动态表Id
+     * 创建时间
      */
-    @Column(name = "table_id")
-    private Integer tableId;
+    @Column(name = "create_time")
+    private Date createTime;
 
     /**
-     * Mysql 数据库 列  类型
+     * 创建用户ID
      */
-    @Column(name = "type_for_mysql")
-    private String typeForMysql;
+    @Column(name = "create_user_id")
+    private Integer createUserId;
+
+    /**
+     * 修改时间
+     */
+    @Column(name = "update_time")
+    private Date updateTime;
+
+    /**
+     * 修改用户ID
+     */
+    @Column(name = "update_user_id")
+    private Integer updateUserId;
 
     /**
      * @return id
@@ -83,150 +102,6 @@ public class DynamicColumnManage {
      */
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    /**
-     * 获取列名
-     *
-     * @return column_name - 列名
-     */
-    public String getColumnName() {
-        return columnName;
-    }
-
-    /**
-     * 设置列名
-     *
-     * @param columnName 列名
-     */
-    public void setColumnName(String columnName) {
-        this.columnName = columnName;
-    }
-
-    /**
-     * 获取创建时间
-     *
-     * @return create_time - 创建时间
-     */
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    /**
-     * 设置创建时间
-     *
-     * @param createTime 创建时间
-     */
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    /**
-     * 获取小数点
-     *
-     * @return decimal_point - 小数点
-     */
-    public Integer getDecimalPoint() {
-        return decimalPoint;
-    }
-
-    /**
-     * 设置小数点
-     *
-     * @param decimalPoint 小数点
-     */
-    public void setDecimalPoint(Integer decimalPoint) {
-        this.decimalPoint = decimalPoint;
-    }
-
-    /**
-     * 获取是否自动递增
-     *
-     * @return is_autoincrement - 是否自动递增
-     */
-    public Boolean getIsAutoincrement() {
-        return isAutoincrement;
-    }
-
-    /**
-     * 设置是否自动递增
-     *
-     * @param isAutoincrement 是否自动递增
-     */
-    public void setIsAutoincrement(Boolean isAutoincrement) {
-        this.isAutoincrement = isAutoincrement;
-    }
-
-    /**
-     * 获取是否为主键
-     *
-     * @return is_primary_key - 是否为主键
-     */
-    public Boolean getIsPrimaryKey() {
-        return isPrimaryKey;
-    }
-
-    /**
-     * 设置是否为主键
-     *
-     * @param isPrimaryKey 是否为主键
-     */
-    public void setIsPrimaryKey(Boolean isPrimaryKey) {
-        this.isPrimaryKey = isPrimaryKey;
-    }
-
-    /**
-     * 获取长度
-     *
-     * @return length - 长度
-     */
-    public Integer getLength() {
-        return length;
-    }
-
-    /**
-     * 设置长度
-     *
-     * @param length 长度
-     */
-    public void setLength(Integer length) {
-        this.length = length;
-    }
-
-    /**
-     * 获取是否为空
-     *
-     * @return nullable - 是否为空
-     */
-    public Boolean getNullable() {
-        return nullable;
-    }
-
-    /**
-     * 设置是否为空
-     *
-     * @param nullable 是否为空
-     */
-    public void setNullable(Boolean nullable) {
-        this.nullable = nullable;
-    }
-
-    /**
-     * 获取备注
-     *
-     * @return remark - 备注
-     */
-    public String getRemark() {
-        return remark;
-    }
-
-    /**
-     * 设置备注
-     *
-     * @param remark 备注
-     */
-    public void setRemark(String remark) {
-        this.remark = remark;
     }
 
     /**
@@ -248,6 +123,24 @@ public class DynamicColumnManage {
     }
 
     /**
+     * 获取列名
+     *
+     * @return column_name - 列名
+     */
+    public String getColumnName() {
+        return columnName;
+    }
+
+    /**
+     * 设置列名
+     *
+     * @param columnName 列名
+     */
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+
+    /**
      * 获取Mysql 数据库 列  类型
      *
      * @return type_for_mysql - Mysql 数据库 列  类型
@@ -263,5 +156,203 @@ public class DynamicColumnManage {
      */
     public void setTypeForMysql(String typeForMysql) {
         this.typeForMysql = typeForMysql;
+    }
+
+    /**
+     * 获取长度
+     *
+     * @return length - 长度
+     */
+    public Integer getLength() {
+        return length;
+    }
+
+    /**
+     * 设置长度
+     *
+     * @param length 长度
+     */
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    /**
+     * 获取小数点
+     *
+     * @return decimal_point - 小数点
+     */
+    public Integer getDecimalPoint() {
+        return decimalPoint;
+    }
+
+    /**
+     * 设置小数点
+     *
+     * @param decimalPoint 小数点
+     */
+    public void setDecimalPoint(Integer decimalPoint) {
+        this.decimalPoint = decimalPoint;
+    }
+
+    /**
+     * 获取枚举值
+     *
+     * @return enum_value - 枚举值
+     */
+    public String getEnumValue() {
+        return enumValue;
+    }
+
+    /**
+     * 设置枚举值
+     *
+     * @param enumValue 枚举值
+     */
+    public void setEnumValue(String enumValue) {
+        this.enumValue = enumValue;
+    }
+
+    /**
+     * 获取是否为空
+     *
+     * @return nullable - 是否为空
+     */
+    public Boolean getNullable() {
+        return nullable;
+    }
+
+    /**
+     * 设置是否为空
+     *
+     * @param nullable 是否为空
+     */
+    public void setNullable(Boolean nullable) {
+        this.nullable = nullable;
+    }
+
+    /**
+     * 获取是否为主键
+     *
+     * @return is_primary_key - 是否为主键
+     */
+    public Boolean getIsPrimaryKey() {
+        return isPrimaryKey;
+    }
+
+    /**
+     * 设置是否为主键
+     *
+     * @param isPrimaryKey 是否为主键
+     */
+    public void setIsPrimaryKey(Boolean isPrimaryKey) {
+        this.isPrimaryKey = isPrimaryKey;
+    }
+
+    /**
+     * 获取是否自动递增
+     *
+     * @return is_autoincrement - 是否自动递增
+     */
+    public Boolean getIsAutoincrement() {
+        return isAutoincrement;
+    }
+
+    /**
+     * 设置是否自动递增
+     *
+     * @param isAutoincrement 是否自动递增
+     */
+    public void setIsAutoincrement(Boolean isAutoincrement) {
+        this.isAutoincrement = isAutoincrement;
+    }
+
+    /**
+     * 获取备注
+     *
+     * @return remark - 备注
+     */
+    public String getRemark() {
+        return remark;
+    }
+
+    /**
+     * 设置备注
+     *
+     * @param remark 备注
+     */
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    /**
+     * 获取创建时间
+     *
+     * @return create_time - 创建时间
+     */
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     * 设置创建时间
+     *
+     * @param createTime 创建时间
+     */
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    /**
+     * 获取创建用户ID
+     *
+     * @return create_user_id - 创建用户ID
+     */
+    public Integer getCreateUserId() {
+        return createUserId;
+    }
+
+    /**
+     * 设置创建用户ID
+     *
+     * @param createUserId 创建用户ID
+     */
+    public void setCreateUserId(Integer createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    /**
+     * 获取修改时间
+     *
+     * @return update_time - 修改时间
+     */
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    /**
+     * 设置修改时间
+     *
+     * @param updateTime 修改时间
+     */
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    /**
+     * 获取修改用户ID
+     *
+     * @return update_user_id - 修改用户ID
+     */
+    public Integer getUpdateUserId() {
+        return updateUserId;
+    }
+
+    /**
+     * 设置修改用户ID
+     *
+     * @param updateUserId 修改用户ID
+     */
+    public void setUpdateUserId(Integer updateUserId) {
+        this.updateUserId = updateUserId;
     }
 }
--- console/src/main/java/com/ssr/console/model/dynamic/DynamicTableManage.java
+++ console/src/main/java/com/ssr/console/model/dynamic/DynamicTableManage.java
@@ -1,12 +1,7 @@
 package com.ssr.console.model.dynamic;
 
 import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Table(name = "dynamic_table_manage")
 public class DynamicTableManage {
@@ -15,10 +10,15 @@ public class DynamicTableManage {
     private Integer id;
 
     /**
-     * 创建时间
+     * 表名
      */
-    @Column(name = "create_time")
-    private Date createTime;
+    @Column(name = "table_name")
+    private String tableName;
+
+    /**
+     * 描述
+     */
+    private String remark;
 
     /**
      * 主键所属列Id
@@ -27,15 +27,28 @@ public class DynamicTableManage {
     private Integer primaryKeyColumnId;
 
     /**
-     * 描述
+     * 创建时间
      */
-    private String remark;
+    @Column(name = "create_time")
+    private Date createTime;
 
     /**
-     * 表名
+     * 创建用户ID
      */
-    @Column(name = "table_name")
-    private String tableName;
+    @Column(name = "create_user_id")
+    private Integer createUserId;
+
+    /**
+     * 修改时间
+     */
+    @Column(name = "update_time")
+    private Date updateTime;
+
+    /**
+     * 修改用户ID
+     */
+    @Column(name = "update_user_id")
+    private Integer updateUserId;
 
     /**
      * @return id
@@ -52,39 +65,21 @@ public class DynamicTableManage {
     }
 
     /**
-     * 获取创建时间
+     * 获取表名
      *
-     * @return create_time - 创建时间
+     * @return table_name - 表名
      */
-    public Date getCreateTime() {
-        return createTime;
+    public String getTableName() {
+        return tableName;
     }
 
     /**
-     * 设置创建时间
+     * 设置表名
      *
-     * @param createTime 创建时间
+     * @param tableName 表名
      */
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    /**
-     * 获取主键所属列Id
-     *
-     * @return primary_key_column_id - 主键所属列Id
-     */
-    public Integer getPrimaryKeyColumnId() {
-        return primaryKeyColumnId;
-    }
-
-    /**
-     * 设置主键所属列Id
-     *
-     * @param primaryKeyColumnId 主键所属列Id
-     */
-    public void setPrimaryKeyColumnId(Integer primaryKeyColumnId) {
-        this.primaryKeyColumnId = primaryKeyColumnId;
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
     }
 
     /**
@@ -106,21 +101,92 @@ public class DynamicTableManage {
     }
 
     /**
-     * 获取表名
+     * 获取主键所属列Id
      *
-     * @return table_name - 表名
+     * @return primary_key_column_id - 主键所属列Id
      */
-    public String getTableName() {
-        return tableName;
+    public Integer getPrimaryKeyColumnId() {
+        return primaryKeyColumnId;
     }
 
     /**
-     * 设置表名
+     * 设置主键所属列Id
      *
-     * @param tableName 表名
+     * @param primaryKeyColumnId 主键所属列Id
      */
-    public void setTableName(String tableName) {
-        this.tableName = tableName;
+    public void setPrimaryKeyColumnId(Integer primaryKeyColumnId) {
+        this.primaryKeyColumnId = primaryKeyColumnId;
     }
 
+    /**
+     * 获取创建时间
+     *
+     * @return create_time - 创建时间
+     */
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     * 设置创建时间
+     *
+     * @param createTime 创建时间
+     */
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    /**
+     * 获取创建用户ID
+     *
+     * @return create_user_id - 创建用户ID
+     */
+    public Integer getCreateUserId() {
+        return createUserId;
+    }
+
+    /**
+     * 设置创建用户ID
+     *
+     * @param createUserId 创建用户ID
+     */
+    public void setCreateUserId(Integer createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    /**
+     * 获取修改时间
+     *
+     * @return update_time - 修改时间
+     */
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    /**
+     * 设置修改时间
+     *
+     * @param updateTime 修改时间
+     */
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    /**
+     * 获取修改用户ID
+     *
+     * @return update_user_id - 修改用户ID
+     */
+    public Integer getUpdateUserId() {
+        return updateUserId;
+    }
+
+    /**
+     * 设置修改用户ID
+     *
+     * @param updateUserId 修改用户ID
+     */
+    public void setUpdateUserId(Integer updateUserId) {
+        this.updateUserId = updateUserId;
+    }
 }
